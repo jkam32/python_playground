6ca2d172243e1fb3bf844524eff9090562bb60b2
JS <jkam32@siswa.um.edu.my>
2018-09-02 20:54:11 +0800

    added ipython/jupyter notebook for easy learning

--- revision_datatypes.py
+++ revision_datatypes.py
@@ -1,1 +1,21 @@
 # crash course revision on fundamental python data types
+
+## Boolean
+# True / False
+
+## Numbers
+
+# Coercing float to int and vice versa
+print(float(2))
+print(int(2.0))
+print(int(2.5)) # truncate
+print(1.134458973459783149857139847534985349578394857) #1.1344589734597832, precision at 16th decimal place
+
+# Some weird number divisions to watch out
+print(11 / 2)
+print(11 // 2) # truncate to integer, if positive
+print(11 // 2) # truncated integer becomes float, with ".0"
+print(-11 // 2) # if negative, round down
+
+#
+
